cb67f38a3327e3142e4f8278e1824baa54618c4e
tassnimelleuch <tasnim.eleuch@enis.tn>
2025-11-16 01:08:10 +0100

    testing pipeline

--- test_model.py
+++ test_model.py
@@ -4,12 +4,10 @@ import cv2
 import numpy as np
 
 def test_docker_model():
-    # Load a test image
     img = np.random.randint(0, 255, (150, 150, 3), dtype=np.uint8)
     _, img_encoded = cv2.imencode('.jpg', img)
     img_base64 = base64.b64encode(img_encoded).decode('utf-8')
     
-    # Test prediction
     response = requests.post(
         'http://localhost:8000/predict',
         json={'image': img_base64}
